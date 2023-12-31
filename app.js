d2be6f393c4f0625761cb4557ac52c14f7353c64
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-01 01:01:54 +0400

    Pull datas with FETCh API

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ const buttonWrapper = document.querySelector(".button-wrapper");
 const searchButton = document.querySelector("#searchButton");
 const clearButton = document.querySelector("#clearButton");
 const imageListWrapper = document.querySelector(".imagelist-wrapper");
+
+runEventListener();
+
+function runEventListener() {
+    form.addEventListener("submit", search);
+
+}
+
+function search(e) {
+    // 
+
+    const searchValue = searchInput.value.trim();
+    fetch(`https://api.unsplash.com/search/photos?query=${searchValue}`, {
+        method: "GET",
+        headers: {
+            Authorization: "Client-ID nts6xI_lt7ibYFWuDNiAAuShRZMg1N9VHcs-n8B8hhY"
+        }
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+
+    e.preventDefault();
+}
