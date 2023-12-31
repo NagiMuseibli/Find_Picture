6a703987d2c10e479f23b9697011261892d7194a
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-01 00:44:37 +0400

    querySelectors on js

---
+++ app.js
@@ -0,0 +1,8 @@
+const formWrapper = document.querySelector('.form-wrapper');
+const form = document.querySelector('#form');
+const searchInput = document.querySelector("#searchInput");
+
+const buttonWrapper = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#searchButton");
+const clearButton = document.querySelector("#clearButton");
+const imageListWrapper = document.querySelector(".imagelist-wrapper");
